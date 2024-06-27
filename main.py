de5b2f539dffd7563b34184b117d955bf0a4073c
Sergio Roa <sergioraul02@hotmail.com>
2024-06-27 10:52:45 -0500

    Actualizar permisos de main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 from routers.transcribe import transcribe_app
 from routers.userlog import userlog_router
 
@@ -7,6 +8,19 @@ app = FastAPI()
 app.title = "Cortitelas API"
 app.version = "0.0.1"
 
+# Configuración de CORS
+origins = [
+    "https://elasistenteia.com/"
+
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(transcribe_app)
 app.include_router(userlog_router)
